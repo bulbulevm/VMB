666160a48a9cd33f91632cbf35f8b982f581692d
Vitthal Bulbule <Vitthal Bulbule@DESKTOP-FU9S1DA>
2018-05-15 11:10:10 +0530

    fourth commit

---
+++ src/com/tesm/demo/Demo4.java
@@ -0,0 +1,5 @@
+package com.tesm.demo;
+
+public class Demo4 {
+
+}
---
+++ src/com/tesm/test/Test1.java
@@ -0,0 +1,13 @@
+package com.tesm.test;
+
+import org.testng.annotations.Test;
+
+public class Test1 
+{
+
+	@Test
+	public void testvts()
+	{
+		System.out.println("nau naby gharls");
+	}
+}
