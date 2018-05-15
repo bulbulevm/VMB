0772c467bd2cd2cbf8c6eac9a23c8ba64c7e827f
Vitthal Bulbule <Vitthal Bulbule@DESKTOP-FU9S1DA>
2018-05-15 10:51:11 +0530

    second commit

--- src/com/tesm/demo/Demo1.java
+++ src/com/tesm/demo/Demo1.java
@@ -2,11 +2,12 @@ package com.tesm.demo;
 
 import org.testng.annotations.Test;
 
-public class Demo1 
+public class Demo1
 {
-@Test
-public void print()
-{
-	System.out.println("ewwwwwww");
-}
+	@Test
+	public void print()
+	{
+		System.out.println("eww");
+	}
+
 }
